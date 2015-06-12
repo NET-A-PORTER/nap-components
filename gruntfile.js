4efc60d971322feb47639fd3c98845a139e85191
Matthew Green <matthew.green@net-a-porter.com>
2015-06-12 16:56:31 +0100

    No need to hash - let client do it

--- gruntfile.js
+++ gruntfile.js
@@ -3,15 +3,9 @@
 module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-filerev');
 
 	grunt.initConfig({
 
-		clean: {
-			elements: ['elements*.html']
-		},
-	  
 	  	concat: {
 		    dist: {
 		      	src: [
@@ -22,22 +16,9 @@ module.exports = function(grunt) {
 		    },
 	  	},
 
-	  	filerev: {
-	  		options: {
-	            algorithm: 'md5',
-	            length: 16
-			},
-			elements: {
-	            src: ['elements.html'],
-	            dest: '.'
-			}
-	  	}
-
 	});
 
 	grunt.registerTask('default', [
-		'clean',
-		'concat',
-		'filerev'
+		'concat'
 	])
 };
