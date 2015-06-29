3e429475b7d9dbf5fcec697bccde8b0558196a46
Matthew Green <matthew.green@net-a-porter.com>
2015-06-29 11:37:07 +0100

    Disable plugins by default

--- wct.conf.js
+++ wct.conf.js
@@ -1,14 +1,16 @@
 module.exports = {
   plugins: {
-    // sauce: {
-    //   browsers: [
-    //   	"OS X 10.10/chrome",
-    //     "OS X 10.10/firefox",
-    //   	'OS X 10.10/safari',
-    //   	'Windows 8.1/internet explorer'
-  	 //  ]
-    // }
+    sauce: {
+      disabled: true,
+      browsers: [
+      	"OS X 10.10/chrome",
+        "OS X 10.10/firefox",
+      	'OS X 10.10/safari',
+      	'Windows 8.1/internet explorer'
+  	  ]
+    },
     local: {
+      disabled: true,
       browsers: ["chrome"]
     }
   },
