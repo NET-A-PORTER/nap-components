114bab0a1db7535a6e4cc2a91cf9703105498224
Matthew Green <matthew.green@net-a-porter.com>
2015-06-29 09:27:46 +0100

    Trying local WCT on Travis

--- wct.conf.js
+++ wct.conf.js
@@ -1,16 +1,16 @@
 module.exports = {
   plugins: {
-    sauce: {
-      browsers: [
-      	"OS X 10.10/chrome",
-        "OS X 10.10/firefox",
-      	'OS X 10.10/safari',
-      	'Windows 8.1/internet explorer'
-  	  ]
-    }
-    // local: {
-    //   browsers: ["chrome"]
+    // sauce: {
+    //   browsers: [
+    //   	"OS X 10.10/chrome",
+    //     "OS X 10.10/firefox",
+    //   	'OS X 10.10/safari',
+    //   	'Windows 8.1/internet explorer'
+  	 //  ]
     // }
+    local: {
+      browsers: ["chrome"]
+    }
   },
 };
 
